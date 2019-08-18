b62082320b06aba3f0f7cbf3f88f0189d50ad5e0
lxj <lxj@DESKTOP-NPIH7R7>
2019-08-18 22:24:15 +1000

    prototype

---
+++ src/main/java/asd/demo/model/Property.java
@@ -0,0 +1,14 @@
+package com.uts.asd.entity;
+
+public class Property {
+	private Integer id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+}
