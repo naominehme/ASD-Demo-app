db813b9ddec9c261c4f3abbe1f6921877beb1edd
Harold Seefeld <seefeld.harold@gmail.com>
2019-09-03 00:19:51 +1000

    test tomcat server run

--- src/main/java/com/uts/asd/AsdApplication.java
+++ src/main/java/com/uts/asd/AsdApplication.java
@@ -5,13 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.github.fabiomaffioletti.firebase.EnableFirebaseRepositories;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.io.IOException;
 
 @SpringBootApplication
 @EnableFirebaseRepositories
 @MapperScan(basePackages = "com.uts.asd.mapper")
-public class AsdApplication {
+public class AsdApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(AsdApplication.class, args);
