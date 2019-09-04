9e8fb21404474e6b2b3ac03ec14ed977878c036a
Harold Seefeld <seefeld.harold@gmail.com>
2019-09-05 00:23:39 +1000

    display preferences by default

--- src/main/webapp/js/populate-watchlist.js
+++ src/main/webapp/js/populate-watchlist.js
@@ -20,9 +20,9 @@ var populateWatchlistPropertyPreferences = function(elementID, customerID) {
         content = ""
         for(i=0; i<data.length; i++){
             content += '<div class="content-watchlist-preference">'
-            content += '<img border="0" alt="24 Yorkshire Street" src="https://www.rawsonhomes.com.au/-/media/rawson-homes/home-designs/bronte/facades/hi-res-bronte-hamptons-2168x1355.ashx?thn=0&w=1016&hash=96A840845863F5C11C9429D577F0F76C" width="100" height="100">';
-            content += "<p>" + data[i].propertyID + "</p>"
-            content += '<a class="remove-watchlist-item" href="removePropertyFromWatchlist?propertyID=' + data[i].propertyID + '&customerID=' + customerID +'">Remove from watchlist</a>'
+           // content += '<img border="0" alt="24 Yorkshire Street" src="https://www.rawsonhomes.com.au/-/media/rawson-homes/home-designs/bronte/facades/hi-res-bronte-hamptons-2168x1355.ashx?thn=0&w=1016&hash=96A840845863F5C11C9429D577F0F76C" width="100" height="100">';
+            content += "<p>" + data[i].preferenceID + "</p>"
+            //content += '<a class="remove-watchlist-item" href="removePropertyFromWatchlist?propertyID=' + data[i].propertyID + '&customerID=' + customerID +'">Remove from watchlist</a>'
             content += "</div>"
         }
         $(elementID).append(content);
