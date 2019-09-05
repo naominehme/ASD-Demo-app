8d18fefc3036f9fa3073287cb0f5be4d40d9edbe
weixianggao <wale@WaledeMacBook-Pro.local>
2019-09-05 23:04:18 +1000

    interface update

---
+++ Desktop/ASD/ASD-Demo-app-master-2/src/main/java/com/uts/asd/entity/Property.java
@@ -0,0 +1,163 @@
+package com.uts.asd.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.github.fabiomaffioletti.firebase.document.FirebaseDocument;
+import com.github.fabiomaffioletti.firebase.document.FirebaseId;
+
+@FirebaseDocument("/Property")
+@JsonIgnoreProperties
+public class Property {
+	
+	@FirebaseId
+	private Integer id;
+	@JsonProperty("address")
+	private String address;
+	@JsonProperty("bedroom")
+	private Integer bedroom;
+	@JsonProperty("bathroom")
+	private Integer bathroom;
+	@JsonProperty("garage")
+	private Integer garage;
+	@JsonProperty("state")
+	private Integer state;
+	@JsonProperty("url")
+	private String url;
+	@JsonProperty("description")
+	private String description;
+	@JsonProperty("title")
+	private String title;
+	@JsonProperty("sqm")
+	private Integer sqm;
+	
+	@JsonIgnore
+	private Auction auction;
+	private List<Bid> bid = new ArrayList<Bid>();
+	private List<String[]> list = new ArrayList<String[]>();
+	
+	public void setBid(List<Bid> bid) {
+		this.bid = bid;
+	}
+
+	public List<Bid> getBid() {
+		return bid;
+	}
+
+	public void setAuction(Auction auction) {
+		this.auction = auction;
+	}
+
+	public Auction getAuction() {
+		return auction;
+	}
+
+	public List<String[]> getList() {
+		return list;
+	}
+
+	public Integer getSqm() {
+		return sqm;
+	}
+
+	public void setSqm(Integer sqm) {
+		this.sqm = sqm;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Integer getState() {
+		return state;
+	}
+
+	public void setState(Integer state) {
+		this.state = state;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Integer getBedroom() {
+		return bedroom;
+	}
+
+	public void setBedroom(Integer bedroom) {
+		this.bedroom = bedroom;
+	}
+
+	public Integer getBathroom() {
+		return bathroom;
+	}
+
+	public void setBathroom(Integer bathroom) {
+		this.bathroom = bathroom;
+	}
+
+	public Integer getGarage() {
+		return garage;
+	}
+
+	public void setGarage(Integer garage) {
+		this.garage = garage;
+	}
+
+	public Property(Integer id) {
+		super();
+		this.id = id;
+	}
+	
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public Property(Integer id, String address, Integer bedroom, Integer bathroom, Integer garage, Integer state,String title,Integer sqm) {
+		super();
+		this.id = id;
+		this.address = address;
+		this.bedroom = bedroom;
+		this.bathroom = bathroom;
+		this.garage = garage;
+		this.state = state;
+		this.title = title;
+		this.sqm = sqm;
+	}
+
+	public Property() {
+		super();
+	}
+	
+}
