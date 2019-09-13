f841f08c562def111e6d6992ed73b16956d49cff
naomi nehme <naomi.nehme@student.uts.edu.au>
2019-09-13 22:48:19 +1000

    fix

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -3,12 +3,21 @@ context('Actions', () => {
     cy.visit('https://asd-demo-app-249308.appspot.com/login.html')
   })
 
-  it('incorrect details', () => {
-    const username = 'test123';
-    const password = '123';
-    cy.get('input[id=username]').type(username);
-    cy.get('input[id=password]').type(password);
-    cy.get('button[id=login]').click();
-    cy.contains('USER NOT FOUND');
-  })
+  it('User Not Found', () => {
+  const username = 'test123';
+  const password = '123';
+  cy.get('input[id=username]').type(username);
+  cy.get('input[id=password]').type(password);
+  cy.get('button[id=login]').click();
+  cy.contains('User Not Found')
+})
+
+it('User Found and Logged in', () => {
+  const username = 'naomi12';
+  const password = 'password123';
+  cy.get('input[id=username]').type(username);
+  cy.get('input[id=password]').type(password);
+  cy.get('button[id=login]').click();
+  cy.contains('Naomi')
+})
 })
