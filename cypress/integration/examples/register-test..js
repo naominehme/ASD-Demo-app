6f679245444df58836d0af63a3c9b54dbea199b3
weixianggao <49175699+weixianggao@users.noreply.github.com>
2019-09-14 19:01:29 +1000

    Update register-test..js

--- cypress/integration/examples/register-test..js
+++ cypress/integration/examples/register-test..js
@@ -2,51 +2,14 @@ context('Register test', () => {
     beforeEach(() => {
         cy.visit('https://asd-demo-app-249308.appspot.com/Register.html')
     })
-    describe('Validation', () => {
-        describe('Required Field', () => {
 
+        describe('Required Field', () => {
 
             it('should show missing required field, on send without any input', () => {
                 cy.get('button[type=submit]')
                     .click()
             })
 
-            it('should show missing required field, on insert nothing to them', () => {
-                cy.get('input[name=firstName]')
-                    .oFocus_1()
-                    .oBlur_2()
-                cy.get('input[name=lastName]')
-                    .oFocus_1()
-                    .oBlur_2()
-                cy.get('input[name=email]')
-                    .oFocus_1()
-                    .oBlur_2()
-                cy.get('input[name=phone]')
-                    .oFocus_1()
-                    .oBlur_2()
-                cy.get('input[name=DOB]')
-                    .oFocus_1()
-                    .oBlur_2()
-                cy.get('input[name=streetName]')
-                    .oFocus_1()
-                    .oBlur_2()
-                cy.get('input[name=streetNumber]')
-                    .oFocus_1()
-                    .oBlur_2()
-                cy.get('input[name=postcode]')
-                    .oFocus_1()
-                    .oBlur_2()
-                cy.get('input[name=state]')
-                    .oFocus_1()
-                    .oBlur_2()
-                cy.get('input[name=username]')
-                    .oFocus_1()
-                    .oBlur_2()
-                cy.get('input[name=password]')
-                    .oFocus_1()
-                    .oBlur_2()
-
-            })
         })
         describe('Email', () => {
             it('should show invalid email', () => {
@@ -76,53 +39,37 @@ context('Register test', () => {
                     .blur()
             })
         })
-    })
-    describe('Success', () => {
-        beforeEach(() => {
+        describe('Register Succeed', () => {
+            it('Register Succeed', () => {
 
-            cy.server()
-            
-            cy.route('GET', 'https://asd-demo-app-249308.appspot.com/Register.html', 'fixture:users.json')
-                .as('registerRequest')
+            const firstName = 'weixiang';
+            const lastName = 'gao';
+            const email = '1@gmail.com';
+            const phone = '0420000000';
+            const DOB = '2011-11-11';
+            const streetName = 'PittSt';
+            const streetNumber = '12';
+            const postcode = '2000';
+            const state = 'NSW';
+            const username = 'test123';
+            const password = '123';
+
+            cy.get('input[name=firstName]').type(firstName);
+            cy.get('input[name=lastName]').type(lastName);
+            cy.get('input[name=email]').type(email);
+            cy.get('input[name=phone]').type(phone);
+            cy.get('input[name=DOB]').type(DOB);
+            cy.get('input[name=streetName]').type(streetName);
+            cy.get('input[name=streetNumber]').type(streetNumber);
+            cy.get('input[name=postcode]').type(postcode);
+            cy.get('input[name=state]').type(state);
+            cy.get('input[name=username]').type(username);
+            cy.get('input[name=password]').type(password);
+            cy.get('button[type=submit]').click();
+
+            })
         })
 
-        it('should redirect to success landing and show welcome message correctly', () => {
-            cy.fixture('formData.json')
-                .then((user) => {
-                    cy.get('input[name=firstName]')
-                        .type(user.firstName)
-                    cy.get('input[name=lastName]')
-                        .type(user.lastName)
-                    cy.get('input[name=phone]')
-                        .type(user.phone)
-                    cy.get('input[name=email]')
-                        .type(user.email)
-                    cy.get('input[name=DOB]')
-                        .type(user.DOB)
-                    cy.get('input[name=streetName]')
-                        .type(user.streetName)
-                    cy.get('input[name=streetNumber]')
-                        .type(user.streetNumber)
-                    cy.get('input[name=postcode]')
-                        .type(user.postcode)
-                    cy.get('input[name=state]')
-                        .type(user.state)
-                    cy.get('input[name=username]')
-                        .type(user.username)
-                    cy.get('input[name=password]')
-                        .type(user.password)
-                    cy.get('button[type=submit]')
-                        .click()
-
-                    cy.wait('@registerRequest')
-                        .then(({ request }) => {
-                            expect(request.body).to.deep.equals(user)
-                            cy.url().should('equals', `${Cypress.config().baseUrl}/success`)
-                            cy.contains('Welcome ')
-                        })
-                })
-        })
-    })
 })
 
 
