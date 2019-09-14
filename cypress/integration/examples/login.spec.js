ce8025495d422cb746c83f92c5257ba41e3da069
naomi nehme <naomi.nehme@student.uts.edu.au>
2019-09-14 11:14:12 +1000

    acceptance tests

---
+++ cypress/integration/examples/login.spec.js
@@ -0,0 +1,35 @@
+context('Login Function', () => {
+  beforeEach(() => {
+    cy.visit('https://asd-demo-app-249308.appspot.com/login.html')
+  })
+
+  it('User Not Found', () => {
+  const username = 'test123';
+  const password = '123';
+  cy.get('input[id=username]').type(username);
+  cy.get('input[id=password]').type(password);
+  cy.get('button[id=login]').click();
+  cy.contains('User Not Found')
+})
+  it('User Forgot Password', () => {
+    const username = 'matty12';
+    const dob = '2000-03-31';
+    const password = '123ee';
+    cy.contains('Forgot Password').click()
+    cy.get('input[id=username]').type(username);
+    cy.get('input[id=dob]').type(dob);
+    cy.get('input[id=password]').type(password);
+    cy.contains('Reset Password').click()
+    cy.contains('Password successfully reset')
+  })
+it('User Found and Logged in and Logged Out', () => {
+  const username = 'naomi12';
+  const password = 'password123';
+  cy.get('input[id=username]').type(username);
+  cy.get('input[id=password]').type(password);
+  cy.get('button[id=login]').click();
+  cy.contains('Naomi')
+  cy.contains('Logout').click()
+
+})
+})
--- src/test/js/login.spec.js
+++ src/test/js/login.spec.js
@@ -1,14 +1,35 @@
-context('Actions', () => {
+context('Login Function', () => {
     beforeEach(() => {
-        cy.visit('http://localhost:63342/ASD-Demo-app/src/main/webapp/login.html?_ijt=diu80sgve4s7bea08ld6ep5ivf')
+        cy.visit('https://asd-demo-app-249308.appspot.com/login.html')
     })
 
-    it('incorrect details', () => {
+    it('User Not Found', () => {
         const username = 'test123';
         const password = '123';
         cy.get('input[id=username]').type(username);
         cy.get('input[id=password]').type(password);
         cy.get('button[id=login]').click();
-        cy.contains('USER NOT FOUND');
+        cy.contains('User Not Found')
+    })
+    it('User Forgot Password', () => {
+        const username = 'matty12';
+        const dob = '2000-03-31';
+        const password = '123ee';
+        cy.contains('Forgot Password').click()
+        cy.get('input[id=username]').type(username);
+        cy.get('input[id=dob]').type(dob);
+        cy.get('input[id=password]').type(password);
+        cy.contains('Reset Password').click()
+        cy.contains('Password successfully reset')
+    })
+    it('User Found and Logged in and Logged Out', () => {
+        const username = 'naomi12';
+        const password = 'password123';
+        cy.get('input[id=username]').type(username);
+        cy.get('input[id=password]').type(password);
+        cy.get('button[id=login]').click();
+        cy.contains('Naomi')
+        cy.contains('Logout').click()
+
     })
 })
