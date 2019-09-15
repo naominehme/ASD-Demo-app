a4e8d8b8399497a86072e27bc8d654244b397126
LeoZzw <49840067+LeoZzw@users.noreply.github.com>
2019-09-16 01:50:48 +1000

    //

--- src/main/java/com/uts/asd/controller/PropertyController.java
+++ src/main/java/com/uts/asd/controller/PropertyController.java
@@ -9,9 +9,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +26,7 @@ import com.uts.asd.service.ActionService;
 import com.uts.asd.service.BidService;
 import com.uts.asd.service.PropertyService;
 import com.uts.asd.util.JsonUtil;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PropertyController {
@@ -33,6 +36,8 @@ public class PropertyController {
 	private ActionService actionService;
 	@Autowired
 	private BidService bidService;
+	
+	int theid;
 	
 	@RequestMapping("/test")
 	public String test(HttpServletRequest request,HttpServletResponse response) throws IOException {
@@ -81,45 +86,115 @@ public class PropertyController {
 		return "PropertyDetail";
 	}
 	
-	@RequestMapping("/backendAdd")
-	public String backendAdd(HttpServletRequest request,HttpServletResponse response) throws IOException {
-		Gson gson = new Gson();
-		try {
-			 //String id = request.getParameter("id");
-            String address = request.getParameter("address");             
-	         String bedroom = request.getParameter("bedroom");
-            String bathroom = request.getParameter("bathroom");
-	         String garage = request.getParameter("garage");
-            String state = request.getParameter("state");
-            String title = request.getParameter("title");
-            String sqm = request.getParameter("sqm");
-            String suburb = request.getParameter("suburb");
-           
-           int bedroomInt = Integer.parseInt(bedroom);
-           int bathroomInt = Integer.parseInt(bathroom);
-           int garageInt = Integer.parseInt(garage);
-           int stateInt = Integer.parseInt(state);
-           int sqmInt = Integer.parseInt(sqm);
-           
-			Property p = new Property();
-			String json = JsonUtil.readJsonFile("src/main/resources/increment.json");
-			Increment i =gson.fromJson(json, Increment.class);
-			p.setId(i.getPropertyid());                   
-			i.setBidid(i.getBidid()+1);
-			String a2 = gson.toJson(i);
-			JsonUtil.writeJsonFile(a2, "src/main/resources/increment.json");
-		    p.setAddress(address);
-		    p.setBedroom(bedroomInt);
-		    p.setBathroom(bathroomInt);
-		    p.setGarage(garageInt);
-		    p.setState(stateInt);
-		    p.setTitle(address);
-		    p.setSqm(sqmInt);
-		    p.setSuburb(suburb);
-		    propertyService.addProperty(p);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+	
+	
+	
+	
+	
+    @RequestMapping(value = "/theProperty", method = RequestMethod.GET)
+    public String theProperty(HttpServletRequest request, HttpServletResponse response, Model model, Property property) 
+            throws IOException {
+		try {		
+		      List<Property> theList = propertyService.searchAll(property);		
+	       	  request.setAttribute("theList", theList);		
+	        }catch (Exception e) {
+			 e.printStackTrace();
+		    }
+		     return "/Property";
+       }
+   
+    
+    @RequestMapping("/addProperty")
+    public ModelAndView  propertyAdd(HttpServletRequest request, HttpServletResponse response, Model model) throws IOException {
+    	Gson gson = new Gson();
+    	try {
+    		String address = request.getParameter("address");             
+    		String bedroom = request.getParameter("bedroom");
+    		String bathroom = request.getParameter("bathroom");
+    		String garage = request.getParameter("garage");
+    		String state = request.getParameter("state");
+    		String title = request.getParameter("title");
+    		String sqm = request.getParameter("sqm");
+    		String suburb = request.getParameter("suburb");
+    		String url = request.getParameter("url");
+    		
+    		int bedroomInt = Integer.parseInt(bedroom);
+    		int bathroomInt = Integer.parseInt(bathroom);
+    		int garageInt = Integer.parseInt(garage);
+    		int stateInt = Integer.parseInt(state);
+    		int sqmInt = Integer.parseInt(sqm);
+    		
+    		Property p = new Property();	
+    		String json = JsonUtil.readJsonFile("src/main/resources/increment.json");
+    		Increment i =gson.fromJson(json, Increment.class);
+    		p.setId(i.getPropertyid());                   
+    		i.setPropertyid(i.getPropertyid()+1);
+    		String a2 = gson.toJson(i);
+    		JsonUtil.writeJsonFile(a2, "src/main/resources/increment.json");
+    		p.setAddress(address);
+    		p.setBedroom(bedroomInt);
+    		p.setBathroom(bathroomInt);
+    		p.setGarage(garageInt);
+    		p.setState(stateInt);
+    		p.setTitle(title);
+    		p.setSqm(sqmInt);
+    		p.setSuburb(suburb);
+    		p.setUrl(url);
+    		propertyService.addProperty(p);
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    	return new ModelAndView("redirect:/theProperty");
+    }   
+    
+    
+    @RequestMapping("/update/{id}")
+    public void update(HttpServletRequest request, HttpServletResponse response, @PathVariable("id") Integer id) 
+           throws IOException {    	
+    	try {
+    		  theid = id;
+       	} catch (Exception e) {
+          e.printStackTrace();
+    	}
+    	response.sendRedirect("/Property_update.html");
+        }
+    
+      
+    @RequestMapping("/updateProperty")
+    public ModelAndView  propertyUpdate(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    	Gson gson = new Gson();
+    	try {
+    		String address = request.getParameter("address");             
+    		String bedroom = request.getParameter("bedroom");
+    		String bathroom = request.getParameter("bathroom");
+    		String garage = request.getParameter("garage");
+    		String state = request.getParameter("state");
+    		String title = request.getParameter("title");
+    		String sqm = request.getParameter("sqm");
+    		String suburb = request.getParameter("suburb");
+    		String url = request.getParameter("url");
+    		
+    		int bedroomInt = Integer.parseInt(bedroom);
+    		int bathroomInt = Integer.parseInt(bathroom);
+    		int garageInt = Integer.parseInt(garage);
+    		int stateInt = Integer.parseInt(state);
+    		int sqmInt = Integer.parseInt(sqm);
+    		
+    		Property p = new Property(theid);
+    		p.setAddress(address);
+    		p.setBedroom(bedroomInt);
+    		p.setBathroom(bathroomInt);
+    		p.setGarage(garageInt);
+    		p.setState(stateInt);
+    		p.setTitle(title);
+    		p.setSqm(sqmInt);
+    		p.setSuburb(suburb);
+    		p.setUrl(url);
+    		propertyService.addProperty(p);
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    	return new ModelAndView("redirect:/theProperty");
+    }   
+	
 }
