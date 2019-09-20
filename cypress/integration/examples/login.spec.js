dbd4739df37b874573ad4838e12594c4dd348fc2
naomi nehme <naomi.nehme@student.uts.edu.au>
2019-09-20 16:13:36 +1000

    test

--- cypress/integration/examples/login.spec.js
+++ cypress/integration/examples/login.spec.js
@@ -28,6 +28,5 @@ it('User Found and Logged in and Logged Out', () => {
   cy.get('input[id=username]').type(username);
   cy.get('input[id=password]').type(password);
   cy.get('button[id=login]').click();
-  cy.contains('Logout').click()
 })
 })
